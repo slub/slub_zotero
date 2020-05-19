2c16677bdffb1cf36af0a396af87b3fe803421e9
Norman Steger <norman.steger@slub-dresden.de>
2020-05-19 13:25:35 +0200

    fix wizard icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,25 +17,26 @@ call_user_func(
             ]
         );
 
-    // wizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    zoterobib {
-                        icon = ' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($extKey) . 'Resources/Public/Icons/user_plugin_zoterobib.svg
-                        title = LLL:EXT:slub_zotero/Resources/Private/Language/Backend.xlf:wizard.title
-                        description = LLL:EXT:slub_zotero/Resources/Private/Language/Backend.xlf:wizard.description
-                        tt_content_defValues {
-                            CType = list
-                            list_type = slubzotero_zoterobib
+        // wizards
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            'mod {
+                wizards.newContentElement.wizardItems.plugins {
+                    elements {
+                        zoterobib {
+                            icon = EXT:slub_zotero/Resources/Public/Icons/user_plugin_zoterobib.svg
+                            title = LLL:EXT:slub_zotero/Resources/Private/Language/Backend.xlf:wizard.title
+                            description = LLL:EXT:slub_zotero/Resources/Private/Language/Backend.xlf:wizard.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = slubzotero_zoterobib
+                            }
                         }
                     }
+                    show = *
                 }
-                show = *
-            }
-       }'
-    );
+        }'
+        );
+        print_r( \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extKey) );
     },
     $_EXTKEY
 );
