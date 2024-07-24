ea782225d83f55f45879360dda2f19ef1faaa297
chrode <christof@rodejohann.de>
2024-07-24 20:53:05 +0200

    Align version contraint with composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
